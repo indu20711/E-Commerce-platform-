fb8c16ee23df5c477897c78ed7a5e286a7382900
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-03-21 10:10:15 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
