524d8d872dc56792191cb1322664ecdf60d182d3
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-03-22 10:07:26 +0530

    User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
+cconst bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
 });
 
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 module.exports = mongoose.model('User', userSchema);
